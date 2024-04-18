c32dc1ad77a4f65b672c8cbf26ee88bc0cb6a3f5
yngibaes <mariasssa21@gmail.com>
2024-04-18 13:17:41 -0500

    Segundo día

--- reactnormal/src/App.js
+++ reactnormal/src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
-/* import ReactDom from 'react-dom'; */
+
+/*Componentes*/
+import Clase from './components/comClase'; //Importar el componente Clase.
+import img from './components/img/hector.jpg' //Importar la imagen.
 
 
 
 function App() {
-  /* const name = 'Ana'; */
-  const user ={
-    name: 'Ana',
-    surname: 'Amaya',
-    age: 18
-  }
-  const element = <h1>Hola, {user.name}</h1>;
-  return (
-    element
-  );
+  return(
+    <>
+        <Clase name='Anita' img={img}/>
+        <Clase name='Anthony' img="https://uvn-brightspot.s3.amazonaws.com/assets/vixes/btg/curiosidades.batanga.com/files/8-sorprendentes-cosas-que-no-sabias-sobre-los-gatos-6.jpg"/>
+        <Clase name='Santiago' img="https://i.ytimg.com/vi/bg9944Rl0Ak/maxresdefault.jpg"/>
+    </>
+  )
 }
 
 export default App;
---
+++ reactnormal/src/components/comClase.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Component } from 'react'
+
+const ThemeContext = React.createContext(null); //Crear un contexto.
+
+class Card extends Component{ //Crear una clase.
+    static contextType = ThemeContext; //Volver estático el contexto.
+    render(){ //Que se va a mostrar en la pantalla.
+        const theme = this.context //Acceder al contexto.
+        const className = theme === 'dark' ? 'bg-gray-800 text-white' : 'bg-white' // Usar clases de Tailwind.
+        return(
+            <div className={className}>
+                <div className="p-4 w-1/2">
+                    <h1 className='text-2xl font-bold'>Hola, {this.props.name}</h1> {/* This.props es un accesorio. */}
+                    <img src={this.props.img} alt={this.props.name} className='w-10 h-auto'/>
+                </div>
+            </div>
+        )
+    }
+}
+
+// Crear un componente proveedor de tema
+class Clase extends Component {
+    render() {
+      // Pasar el estado actual del tema como valor del contexto
+      return (
+        <ThemeContext.Provider value="dark">
+          <Card name={this.props.name} img={this.props.img}/>
+        </ThemeContext.Provider>
+      );
+    }
+  }
+export default Clase;
