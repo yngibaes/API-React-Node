d6c657078ffe06f8af169fd6753ced81b511c625
yngibaes <mariasssa21@gmail.com>
2024-07-02 23:08:16 -0500

    02.07

--- APP/src/config.js
+++
@@ -1,3 +0,0 @@
-const url = 'http://localhost:8000/api'; //URL de la API.
-
-export default url
---
+++ src/config.js
@@ -0,0 +1,3 @@
+const url = 'api-production-bf5e.up.railway.app/api'; //URL de la API.
+
+export default url
